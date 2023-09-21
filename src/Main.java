d38254a391e51533a346283f8f07225970d6daaa
Mr-john056 <ekulakov056@gmail.com>
2023-09-21 22:44:35 +0500

    Create new branch and class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Car car = new Car();
+        Car car2 = new Car();
+        car.modelName = "car1";
+        car2.modelName = "car2";
+        car.wheelsCount = 4;
+        car2.wheelsCount = 4;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Truck truck = new Truck();
+        Truck truck2 = new Truck();
+        truck.modelName = "truck1";
+        truck2.modelName = "truck2";
+        truck.wheelsCount = 6;
+        truck2.wheelsCount = 8;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Bicycle bicycle = new Bicycle();
+        Bicycle bicycle2 = new Bicycle();
+        bicycle.modelName = "bicycle1";
+        bicycle2.modelName = "bicycle2";
+        bicycle.wheelsCount = 2;
+        bicycle2.wheelsCount = 2;
+
+        ServiceStation station = new ServiceStation();
+        station.check(car, null, null);
+        station.check(car2, null, null);
+        station.check(null, bicycle, null);
+        station.check(null, bicycle2, null);
+        station.check(null, null, truck);
+        station.check(null, null, truck2);
         }
     }
-}
